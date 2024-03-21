02ba397c297f9c6c6f32d668e7692f91407c309f
zachee2287 <114746864+nifelix97@users.noreply.github.com>
2024-03-21 12:40:15 +0200

    updates
    updating wishlist functionality

--- lib/pages/wishlist.screen.dart
+++ lib/pages/wishlist.screen.dart
@@ -8,7 +8,7 @@ import 'package:foodapp/widgets/Title.dart';
 import 'package:provider/provider.dart';
 
 class WishlistPage extends StatefulWidget {
-  const WishlistPage({super.key});
+  const WishlistPage({Key? key});
 
   @override
   State<WishlistPage> createState() => _WishlistPageState();
@@ -17,55 +17,128 @@ class WishlistPage extends StatefulWidget {
 class _WishlistPageState extends State<WishlistPage> {
   @override
   Widget build(BuildContext context) {
-    //  --access app cart state
     var wishlistItems = context.watch<WishlistProvider>();
     int totalWishlistItems = wishlistItems.items;
     List<Product> allItems = wishlistItems.allItems;
 
-    allItems.forEach((element) => print(
-        "items in wishlist${element.name} ${element.price} ${element.image}"));
-
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColor,
-          title: myTitle( context, totalQuantity: totalWishlistItems),
-          // backgroundColor: AppColors.backgroundWhite,
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            color: AppColors.backgroundWhite,
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              children: [
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: allItems.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: Image.asset(allItems[index].image),
-                      title: Row(
-                        children: [
-                          Expanded(child: Text(allItems[index].name)),
-                          IconButton(
-                            icon: Icon(Icons.remove),
-                            onPressed: () {
-                              // code to decrease the quantity of the product
-                              context.read<WishlistProvider>().removeProduct(allItems[index]);
-                            },
-                          ),
-                          Text(
-                              '${allItems[index].price} Rwf'), // display the quantity here
-                        ],
-                      ),
-                      subtitle: Text('${allItems[index].price} Rwf'),
-                    );
-                  },
+        title: myTitle(context, totalQuantity: totalWishlistItems),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: AppColors.bgSecondary,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Wishlist',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 10),
+             SizedBox(
+              height: 400, // height of the container
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: allItems.map((product) {
+                    return Card(
+                      margin: EdgeInsets.all(16),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.backgroundWhite,
+                          borderRadius: BorderRadius.circular(25),
+                          border: Border.all(
+                            
+                            color: AppColors.backgroundWhite,
+                          )
+                        ),
+                        width: 180, //  width of the card
+                        height: 250,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.all(Radius.circular(20)),
+                             child:  Image.asset(
+                              product.image,
+                              height: 200, //  height of the image
+                              width: 180, //  width of the image
+                              fit: BoxFit.cover,
+                            ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(product.name,
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                      )
+                                      ),
+                                  IconButton(
+                                    onPressed: () {
+                                      final wishlistProvider =
+                                          context.read<WishlistProvider>();
+                                      if (wishlistProvider.allItems.contains(product)) {
+                                        wishlistProvider.removeProduct(product);
+                                      } else {
+                                        wishlistProvider.addProduct(product);
+                                      }
+                                    },
+                                    icon: Icon(
+                                      wishlistItems.allItems.contains(product)
+                                          ? Icons.favorite
+                                          : Icons.favorite_border,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Text(
+                                'Price,${product.price} Rwf',
+                                style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold,
+                                  color: Colors.green
+                                
+                                ), // font size
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 20), // Adjust height as needed
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Buy Now button onPressed logic here
+                  },
+                  child: Text('Buy Now',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.backgroundWhite),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
