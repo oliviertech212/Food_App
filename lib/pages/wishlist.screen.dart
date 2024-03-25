17a501fc729be9a2af95edba01cd27d6df79882f
zachee2287 <114746864+nifelix97@users.noreply.github.com>
2024-03-25 12:26:15 +0200

    updates
    wishlist updates

--- lib/pages/wishlist.screen.dart
+++ lib/pages/wishlist.screen.dart
@@ -4,7 +4,6 @@ import 'package:foodapp/models/products.model.dart';
 import 'package:foodapp/services/stateMngt/cart.provider.dart';
 import 'package:foodapp/services/stateMngt/wishlist.provider.dart';
 import 'package:foodapp/utils/colors.dart';
-import 'package:foodapp/widgets/PopupMenu.dart';
 import 'package:foodapp/widgets/Title.dart';
 import 'package:provider/provider.dart';
 
@@ -27,12 +26,15 @@ class _WishlistPageState extends State<WishlistPage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
         title: myTitle(context, totalQuantity: totalWishlistItems),
-        // -- handle add menu icon
-        actions: [MyPopUpMenu(context)],
       ),
       body: SingleChildScrollView(
         child: Container(
-          color: AppColors.bgSecondary,
+          decoration: BoxDecoration(
+            color: AppColors.bgSecondary,
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(20.0),
+            ),
+          ),
           height: MediaQuery.of(context).size.height,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -45,8 +47,8 @@ class _WishlistPageState extends State<WishlistPage> {
                 ),
               ),
               const SizedBox(height: 10),
-              SizedBox(
-                height: 400, // height of the container
+             SizedBox(
+              height: 400, // height of the container
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: allItems.map((product) {
@@ -54,43 +56,43 @@ class _WishlistPageState extends State<WishlistPage> {
                       margin: EdgeInsets.all(16),
                       child: Container(
                         decoration: BoxDecoration(
+                          color: AppColors.backgroundWhite,
+                          borderRadius: BorderRadius.circular(25),
+                          border: Border.all(
+                            
                             color: AppColors.backgroundWhite,
-                            borderRadius: BorderRadius.circular(25),
-                            border: Border.all(
-                              color: AppColors.backgroundWhite,
-                            )),
+                          )
+                        ),
                         width: 180, //  width of the card
                         height: 250,
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(20)),
-                              child: Image.asset(
-                                product.image,
-                                height: 200, //  height of the image
-                                width: 180, //  width of the image
-                                fit: BoxFit.cover,
-                              ),
+                              borderRadius: const BorderRadius.all(Radius.circular(20)),
+                             child:  Image.asset(
+                              product.image,
+                              height: 200, //  height of the image
+                              width: 180, //  width of the image
+                              fit: BoxFit.cover,
+                            ),
                             ),
                             Padding(
                               padding: const EdgeInsets.all(8.0),
                               child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text(product.name,
                                       style: const TextStyle(
                                         fontSize: 12,
                                         fontWeight: FontWeight.bold,
-                                      )),
+                                      )
+                                      ),
                                   IconButton(
                                     onPressed: () {
                                       final wishlistProvider =
                                           context.read<WishlistProvider>();
-                                      if (wishlistProvider.allItems
-                                          .contains(product)) {
+                                      if (wishlistProvider.allItems.contains(product)) {
                                         wishlistProvider.removeProduct(product);
                                       } else {
                                         wishlistProvider.addProduct(product);
@@ -107,16 +109,28 @@ class _WishlistPageState extends State<WishlistPage> {
                               ),
                             ),
                             Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
                               child: Text(
-                                'Price,${product.price} Rwf',
+                                'Price:${product.price} Rwf',
                                 style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.green), // font size
+                                  fontSize: 16, fontWeight: FontWeight.bold,
+                                  color: Colors.green
+                                
+                                ), // font size
                               ),
                             ),
+                            Center(
+                              child: ElevatedButton(
+                                onPressed: () {}, 
+                                child: Text('Buy Now',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.backgroundWhite,
+                                  fontSize: 16
+                                ),
+                                ),
+                                ),
+                            )
                           ],
                         ),
                       ),
@@ -124,25 +138,42 @@ class _WishlistPageState extends State<WishlistPage> {
                   }).toList(),
                 ),
               ),
-              const SizedBox(height: 20), // Adjust height as needed
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Buy Now button onPressed logic here
-                  },
-                  child: Text(
-                    'Buy Now',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.backgroundWhite),
-                  ),
-                ),
-              ),
-            ],
+            ]   
           ),
         ),
       ),
-    );
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.bgSecondary,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              }, 
+              icon: const Icon(Icons.home_outlined),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/');
+                }, 
+                icon: const Icon(Icons.person_outline),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/cart');
+                  }, 
+                  icon: const Icon(Icons.shopping_basket_outlined)
+                  ),
+                  label: '',
+                )
+        ],
+      ),
+      );
   }
 }
