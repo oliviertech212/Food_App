94079002fd83f69d573da0bd0b66f208bbff557b
oliviertech212 <oliviertech212@gmail.com>
2024-04-06 22:59:06 +0200

    create order statefulwidget

---
+++ lib/pages/order.screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MyOrder extends StatefulWidget {
+  const MyOrder({super.key});
+
+  @override
+  State<MyOrder> createState() => _MyOrderState();
+}
+
+class _MyOrderState extends State<MyOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
